c7739c4d0fd9097e5720f247e8407b1acf354fcc
rare200327 <435897971@qq.com>
2023-05-01 20:08:14 +0800

    添加项目文件。

---
+++ game.h
@@ -0,0 +1,16 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//头文件的包含
+#include <stdio.h>  
+
+//符号的定义
+#define ROW 10
+#define	COL 10
+
+//函数的声明
+InitBoard(char board[ROW][COL], int row, int col);    //初始化的棋盘
+
+DisplayBoard(char board[ROW][COL], int row, int col);   //打印棋盘
+
+
+
+
---
+++ test.c
@@ -0,0 +1,54 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include "game.h"
+void menu()
+{
+	printf(" 三子棋游戏：                             \n");
+	printf("               1.play                     \n");
+	printf("               0.exit                     \n");
+	printf("                                          \n");
+}
+void Game()
+{   //储存数据 -  二维数组
+	char board[ROW][COL];
+	//初始化棋盘 - 初始化为空格
+	InitBoard(board, ROW, COL);
+	//打印棋盘 - 本质是打印数组的内容
+	DisplayBoard(board, ROW, COL);
+}
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择->:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			Game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+	    default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+
+	} while (input);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
